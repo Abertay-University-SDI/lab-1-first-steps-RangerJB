d30cfdc633d224c30c824aaaafe7e08187a9e8d2
RangerJB <2503792@uad.ac.uk>
2026-02-08 12:09:45 +0000

    Basic collistion and game over Food added and point, time system

--- Coursework/CMP105App/Level.cpp
+++ Coursework/CMP105App/Level.cpp
@@ -9,8 +9,21 @@ Level::Level(sf::RenderWindow& hwnd, Input& in) :
 	// snake in the middle of the screen
 	sf::Vector2 window_size = m_window.getSize();
 	m_snake.setPosition({ window_size.x / 2.f, window_size.y /2.f });
-}
 
+
+	//initialise food 
+	m_food.setRadius(5.f);
+	m_food.setFillColor(sf::Color::Red);
+	spawnFood();
+
+}
+void Level::spawnFood() {
+	sf::Vector2 window_size = m_window.getSize();
+	float window_x = rand() % window_size.x;
+	float window_y = rand() % window_size.y;
+	m_food.setPosition({ window_x,window_y });
+	
+}
 // handle user input
 void Level::handleInput(float dt)
 {
@@ -36,6 +49,10 @@ void Level::handleInput(float dt)
 // Update game objects
 void Level::update(float dt)
 {
+	if (m_isGameover) return;
+
+	m_timeTaken += dt;
+	//move the snake 
 	switch (m_direction) {
 	case snakeDirection::UP:
 		m_snake.move({ 0,-m_snakeSpeed * dt });
@@ -57,15 +74,34 @@ void Level::update(float dt)
 		snake_pos.x +2*m_snake.getRadius()> window_size.x||
 		snake_pos.y + 2 * m_snake.getRadius()> window_size.y) 
 	{
+		m_isGameover = true;
+		std::cout << "Game Over\nPoints: " << m_foodEaten;
+		std::cout << "\nTime taken: " << m_timeTaken;
+
+		//old:
 		//reset to middle
-		m_snake.setPosition({ window_size.x / 2.f, window_size.y / 2.f });
+		//m_snake.setPosition({ window_size.x / 2.f, window_size.y / 2.f });
 	}
+	//check for collisions
+	float radii_sum = m_snake.getRadius() + m_food.getRadius();
+	sf::Vector2f snake_center = m_snake.getPosition() + sf::Vector2f(m_snake.getRadius(), m_snake.getRadius());
+	sf::Vector2f food_center = m_food.getPosition() + sf::Vector2f(m_food.getRadius(), m_food.getRadius());
+	float x_diff = snake_center.x - food_center.x;
+	float y_diff = snake_center.y - food_center.y;
+	if (radii_sum * radii_sum > (x_diff * x_diff) + (y_diff * y_diff)) {
+		m_snakeSpeed *= 1.2f;
+		spawnFood();
+		m_foodEaten++;
+	}
+	
+
 }
 
 // Render level
 void Level::render()
 {
 	beginDraw();
+	m_window.draw(m_food);
 	m_window.draw(m_snake);
 	endDraw();
 }
--- Coursework/CMP105App/Level.h
+++ Coursework/CMP105App/Level.h
@@ -14,10 +14,17 @@ public:
 private:
 	// Default functions for rendering to the screen.
 
+	void spawnFood();
+
+
 	// Default variables for level class.
 	sf::CircleShape m_snake;
 	float m_snakeSpeed = 300.0f;
+	sf::CircleShape m_food;
 
+	int m_foodEaten = 0;
+	float m_timeTaken = 0.f;
+	bool m_isGameover = false;
 
 	enum class snakeDirection {UP,DOWN,LEFT,RIGHT};
 	snakeDirection m_direction;
